851fad7a3cb640297527249f746fee32310ea1f8
Campbell <crc00042@mix.wvu.edu>
2025-06-17 13:52:39 -0600

    update to unified format

--- alexandria/scripts/run_atomgpt_data.sh
+++ alexandria/scripts/run_atomgpt_data.sh
@@ -3,13 +3,6 @@ set -e
 mkdir -p agpt_alexandria
 uv pip install jarvis-tools==2024.4.* pymatgen numpy pandas tqdm
 export DEBUG="true"
-python scripts/id_prop.py \
-    --csv-files dataset1.csv dataset2.csv \
-    --id-key mat_id \
-    --target Tc \
-    --output agpt_alexandria \
-    --seed 123 \
-    --max-size 1000
-
-
-
+python alexandria_preprocess.py atomgpt \
+       --csv-files dataset1.csv dataset2.csv \
+       --output ./atomgpt_data --max-size 1000 --seed 123
