af173d2f2a54be7091407cc393608c9f36c9846b
Campbell <crc00042@mix.wvu.edu>
2025-06-18 11:30:53 -0600

    add rm -rf JVASP*

--- tc_supercon/scripts/run_atomgpt_data.sh
+++ tc_supercon/scripts/run_atomgpt_data.sh
@@ -4,3 +4,4 @@ mkdir -p atomgpt_data
 uv pip install jarvis-tools pymatgen numpy pandas tqdm
 python scripts/data_preprocess.py atomgpt  --dataset dft_3d --output ./atomgpt_data  \
                                            --target Tc_supercon --max-size 1000 --seed 123
+rm -rf JVASP*
