f819ce06df282cad400f0199c0edb1ba5383c8e6
Campbell <crc00042@mix.wvu.edu>
2025-06-20 09:19:45 -0600

    change path

--- job_runs/flowmm_benchmark_jarvis/write_benchmark.sh
+++ job_runs/flowmm_benchmark_jarvis/write_benchmark.sh
@@ -2,9 +2,9 @@
 conda activate flowmm
 
 python inspect_pt.py \
-   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_jarvis/outputs/rfmcsp-conditional-supercon/zjnmz1mi/checkpoints/inferences/consolidated_reconstruct.pt \
+   --pt_path /lab/mml/kipp/677/jarvis/rhys/benchmarks/job_runs/flowmm_benchmark_jarvis/outputs/rfmcsp-conditional-supercon/iku778pz/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv AI-AtomGen-prop-dft_3d-test-rmse.csv
 
 mv ../../models/flowmm/AI-AtomGen-prop-dft_3d-test-rmse.csv .
 
-python plot_error_distribution.py
+python ../plot_error_distribution.py
