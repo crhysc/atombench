7dc57b7f3c7285ab171c583ad6af7508300f3d59
Charles Campbell <crc00042@mix.wvu.edu>
2025-07-02 12:15:57 -0600

    rm anything that has distribution*.pdf

--- job_runs/loop.sh
+++ job_runs/loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for dir in */
 	do cd "$dir"
-		rm -rf distribution.pdf metrics.json
+		rm -rf distribution*.pdf metrics.json
 		python ../../scripts/plot_error_distribution.py #| tail -n 30 > metrics.txt
 		cd ..
 done
