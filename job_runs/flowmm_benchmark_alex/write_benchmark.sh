de14aed6ddb5f5674ea40c1f0a2a75b156c17b70
crhysc <crc00042@mix.wvu.edu>
2025-09-25 12:10:03 -0400

    fix paths

--- job_runs/flowmm_benchmark_alex/write_benchmark.sh
+++ job_runs/flowmm_benchmark_alex/write_benchmark.sh
@@ -3,7 +3,7 @@ conda activate flowmm
 
 rm -f job_runs/flowmm_benchmark_alex/AI-AtomGen*
 
-python inspect_pt.py \
+python job_runs/flowmm_benchmark_alex/inspect_pt.py \
    --pt_path job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-*/????????/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv job_runs/flowmm_benchmark_alex/AI-AtomGen-prop-dft_3d-test-rmse.csv
 
