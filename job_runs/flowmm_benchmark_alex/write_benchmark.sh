73ed3f68375f2badb883386c71cd002681c2244a
crhysc <crc00042@mix.wvu.edu>
2025-09-25 17:47:13 -0400

    fix inspect_pt.py path

--- job_runs/flowmm_benchmark_alex/write_benchmark.sh
+++ job_runs/flowmm_benchmark_alex/write_benchmark.sh
@@ -3,7 +3,7 @@ conda activate flowmm
 
 rm -f job_runs/flowmm_benchmark_alex/AI-AtomGen*
 
-python job_runs/flowmm_benchmark_alex/inspect_pt.py \
+python scripts/inspect_pt.py \
    --pt_path job_runs/flowmm_benchmark_alex/outputs/rfmcsp-conditional-*/????????/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv job_runs/flowmm_benchmark_alex/AI-AtomGen-prop-dft_3d-test-rmse.csv
 
--- job_runs/flowmm_benchmark_jarvis/write_benchmark.sh
+++ job_runs/flowmm_benchmark_jarvis/write_benchmark.sh
@@ -3,7 +3,7 @@ conda activate flowmm
 
 rm -f job_runs/flowmm_benchmark_jarvis/AI-AtomGen*
 
-python job_runs/flowmm_benchmark_jarvis/inspect_pt.py \
+python scripts/inspect_pt.py \
    --pt_path job_runs/flowmm_benchmark_jarvis/outputs/rfmcsp-conditional-*/????????/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv AI-AtomGen-prop-dft_3d-test-rmse.csv
 
