302efd3910ee57996ddbb3b1a330c196ef232d47
crhysc <crc00042@mix.wvu.edu>
2025-09-25 12:09:03 -0400

    fix paths

--- job_runs/flowmm_benchmark_jarvis/write_benchmark.sh
+++ job_runs/flowmm_benchmark_jarvis/write_benchmark.sh
@@ -3,7 +3,7 @@ conda activate flowmm
 
 rm -f job_runs/flowmm_benchmark_jarvis/AI-AtomGen*
 
-python inspect_pt.py \
+python job_runs/flowmm_benchmark_jarvis/inspect_pt.py \
    --pt_path job_runs/flowmm_benchmark_jarvis/outputs/rfmcsp-conditional-*/????????/checkpoints/inferences/consolidated_reconstruct.pt \
    --output_csv AI-AtomGen-prop-dft_3d-test-rmse.csv
 
