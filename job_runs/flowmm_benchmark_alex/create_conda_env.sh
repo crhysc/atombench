a2a4413615507f0f2c960e39f4350118ae8b8e4d
Charles Campbell <crc00042@mix.wvu.edu>
2025-09-25 19:30:45 -0400

    add matminer install

--- job_runs/flowmm_benchmark_alex/create_conda_env.sh
+++ job_runs/flowmm_benchmark_alex/create_conda_env.sh
@@ -6,7 +6,7 @@ mamba env create -f environment.yml -y
 eval "$(conda shell.bash hook)"
 conda activate flowmm
 pip install uv
-uv pip install "jarvis-tools>=2024.5" "pymatgen>=2024.1" pandas numpy tqdm
+uv pip install "jarvis-tools>=2024.5" "pymatgen>=2024.1" pandas numpy tqdm matminer
 uv pip install -e . \
 	       -e remote/riemannian-fm \
 	       -e remote/cdvae \
