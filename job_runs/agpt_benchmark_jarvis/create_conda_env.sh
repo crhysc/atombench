c21b937c1354ab1573ea8ff893c26ee02399d842
C. Rhys Campbell <149001340+crhysc@users.noreply.github.com>
2026-01-11 19:52:05 -0500

    Delete job_runs/agpt_benchmark_jarvis/create_conda_env.sh

--- job_runs/agpt_benchmark_jarvis/create_conda_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd models/atomgpt
-conda create -y -n my_atomgpt python=3.10
-conda run -n my_atomgpt python -m pip install --upgrade pip
-conda run -n my_atomgpt pip install uv
-conda run -n my_atomgpt uv pip install -e .
-
