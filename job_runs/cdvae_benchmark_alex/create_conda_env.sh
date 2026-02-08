47f41b87d6ba11a9a97dcc3f0903d6d2655eabc8
C. Rhys Campbell <149001340+crhysc@users.noreply.github.com>
2026-02-08 16:07:05 -0500

    remove aflow

--- job_runs/cdvae_benchmark_alex/create_conda_env.sh
+++ job_runs/cdvae_benchmark_alex/create_conda_env.sh
@@ -11,5 +11,4 @@ pip install "monty==2022.9.9"
 conda install -c conda-forge "pymatgen>=2022.0.8,<2023" --yes
 pip install pandas jarvis-tools
 pip install --upgrade torch_geometric==1.7.0
-pip install aflow
 pip install -e .
