effd6aa109cb5ed0c56f063d8e1c30221dd74ec0
C. Rhys Campbell <149001340+crhysc@users.noreply.github.com>
2026-02-09 12:52:42 -0500

    conda install -c defaults -c conda-forge mkl=2024.0 --yes

--- job_runs/cdvae_benchmark_alex/create_conda_env.sh
+++ job_runs/cdvae_benchmark_alex/create_conda_env.sh
@@ -8,7 +8,7 @@ conda env create -f env.yml -y
 eval "$(conda shell.bash hook)"
 conda activate cdvae
 conda install -c conda-forge "torchmetrics<0.8" --yes
-conda install mkl=2024.0 --yes
+conda install -c defaults -c conda-forge mkl=2024.0 --yes
 pip install "monty==2022.9.9"
 conda install -c conda-forge "pymatgen>=2022.0.8,<2023" --yes
 pip install pandas jarvis-tools
