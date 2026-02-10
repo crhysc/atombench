aca1f56c4776d38b69d27a69de0b355206f7803a
Charles Campbell <crc00042@mix.wvu.edu>
2026-02-09 22:16:22 -0500

    add print total computation time lines

--- job_runs/flowmm_benchmark_alex/yamls.sh
+++ job_runs/flowmm_benchmark_alex/yamls.sh
@@ -3,6 +3,9 @@
 eval "$(conda shell.bash hook)"
 conda activate flowmm
 
+SECONDS=0
+start_iso="$(date -Iseconds)"
+
 # remove yamls that might exist from past pipeline runs
 rm -f models/flowmm/src/flowmm/rfm/manifolds/stats_supercon*
 rm -f models/flowmm/src/flowmm/rfm/manifolds/stats_alex*
@@ -22,3 +25,11 @@ python -u -m flowmm.rfm.manifolds.spd
 python -u -m flowmm.rfm.manifolds.lattice_params
 python -u -m flowmm.model.standardize data=supercon
 python -u -m flowmm.model.standardize data=alexandria
+
+elapsed="$SECONDS"
+end_iso="$(date -Iseconds)"
+
+printf "Start: %s\nEnd:   %s\nElapsed: %ds (%.2f min, %.2f hr)\n" \
+  "$start_iso" "$end_iso" "$elapsed" \
+  "$(awk "BEGIN{print $elapsed/60}")" \
+  "$(awk "BEGIN{print $elapsed/3600}")"
